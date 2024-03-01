c32a7e4e503a1c0071e6a7c83f40394014f1bdb5
vaishali <vaishalirdj14@gmail.com>
2024-03-01 23:13:11 +0530

    new problems added

--- mergesortProblems/iterativeMergesort/iterativeMergesort.cpp
+++ mergesortProblems/iterativeMergesort/iterativeMergesort.cpp
@@ -3,11 +3,22 @@
 
 using namespace std ;
 
+void merge( vector< int > &arr , int idx , int size ) {
+    int start = idx , end = size - 1 ;
+    while ( start <= end ) {
+        
+    }
+}
+
 int main() {
     vector< int > arr = { 5 , 4 , 3 , 2 , 1 } ;
-    int group = 1 , n = arr.size() ;
+    int group = 2 , n = arr.size() ;
 
-    for ( int i = 0 ; i < n ; i++ ) {
-
+    while ( group < n ) {
+        int size = group ;
+        for ( int i = 0 ; i < n - 1 ; i = i + size ) {
+            merge( arr , i , size ) ;
+        }
+        group *= 2 ;        
     }
 }
---
+++ tests/CC2/Palindrome/paindromeDoublyLinkedList.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Node{
+    int data ;
+    Node *next ;
+    Node *prev ;
+
+    Node();
+    Node( int ) ;
+}
+
+int main() {
+   int n ;
+   cin >> n ;
+
+   
+}
